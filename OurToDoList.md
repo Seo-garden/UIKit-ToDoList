fb7de6d40071336da493487734c3965d2eecb03d
seo-garden <indian4589@naver.com>
2023-10-31 16:44:24 +0900

    1031 ToDoList

--- OurToDoList.md
+++ OurToDoList.md
@@ -1,12 +1,21 @@
 # UIKit ToDoList
 
-유튜브 iOS Company (~5:32) <https://www.youtube.com/watch?v=Vqo36o9fSMM> 
+유튜브 iOS Company <https://www.youtube.com/watch?v=Vqo36o9fSMM> 완료! 
+
+문제점 : 스와이프삭제불가(O) 재부팅해도 삭제된 것을 확인할 수 있다. / 한글 삽입 불가() / 검색 기능()
+
+    #Preview{
+        ViewController() -> 실행하지 않고 결과를 볼 수 있다.
+    }
 
 iOS의 UITableView는 하나의 열에 세로로 스크롤되는 콘텐츠 행들을 표시. 스크롤을 할 수 있는 만큼 UIScrollView 를 상속받고 있다. 
 
-UITableView 란 ? 하나의 열에 세로로 스크롤 되는 콘텐츠 행이다.
+UITableView 란 ? 하나의 열에 세로로 스크롤 되는 콘텐츠 행이다.
 
 UITableViewCell, UITableViewDataSource : UITableView 에 데이터를 넣고 표시하기 위해서는 각 행을 표시해주는 UITableViewCell 과 이를 관리하고 데이터를 적용해줄 수 있는 UITableViewDataSource 가 필요하다. 
 
 UITableViewCell 은 테이블 행의 내용을 관리하도록 도와주는 특수한 유형의 View 이다. 
+
+UserDefaults 는 iOS 및 macOS 앱에서 간단한 데이터를 저장하고 관리하는 데 사용되는 인터페이스이다.
+
 
--- OurToDoList/OurToDoList/ViewController.swift
+++ OurToDoList/OurToDoList/ViewController.swift
@@ -4,32 +4,67 @@
 //
 //  Created by 서정원 on 10/30/23.
 //
-
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-
-
     private let table : UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self,forCellReuseIdentifier: "cell" )
         return table
     }()
+    
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "DoToList"
-        view.addSubview(table)
+        view.addSubview(table)          //제목을 넣어 준다.
         table.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))                  // 우측 상단에 + 버튼을 만들어 준다.
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+    }
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "할일 추가하기", message: "할일을 추가해보세요 !", preferredStyle: .alert)             //
+        alert.addTextField { field in
+            field.placeholder = "할일을 적어보세요!"            //입력칸 뒤에 반투명으로 무엇을 입력해야 할 지 안내하는 문구
+        }
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { [weak self]//확인 버튼
+            (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    //Enter New To Do List
+                    DispatchQueue.main.async {              //재실행을 해도 적용이 된다.
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItems.append(text)
+                        
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
+                        self?.items.append(text)                // 여긴 왜 옵셔널로 줘야 하는가.
+                        self?.table.reloadData()
+                    }
+                }
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))       // 취소 버튼
+        
+        present(alert,animated: true)
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         table.frame = view.bounds
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {       //section 의 행 수
-        return 0
+        return items.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
         return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            UserDefaults.standard.set(items, forKey: "items")           //삭제 후 재부팅을 해도 삭제가 된다.
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
 }
